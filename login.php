334f605546965f9ddbb2b0e7c7025b6dc8a465b5
Guilherme Silveira Froes <100645218+GuixzDev@users.noreply.github.com>
2023-03-01 23:51:20 -0300

    Upando para o git

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "mydatabase";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+  $result = $conn->query($sql);
+
+  $response = array();
+  if ($result->num_rows > 0) {
+    $response["valid"] = true;
+  } else {
+    $response["valid"] = false;
+  }
+
+  echo json_encode($response);
+
+  $conn->close();
+?>
